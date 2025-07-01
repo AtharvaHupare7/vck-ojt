62d259e2de2fbc73a7f820ec011900de7098178b
AtharvaHupare7 <hupareatharv@gmail.com>
2025-07-01 11:29:22 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,37 @@ import CoursesPage from "./pages/CoursesPage"
 import ContactPage from "./pages/ContactPage"
 import AdmissionPage from "./pages/AdmissionPage"
 import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
-// import '/styles/Pages.css'
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import "../src/App.css"
+import { useState } from "react";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
+// import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
     return(
         <>
+        <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Atharv Digambar Hupare"
+          studentPhotoUrl="/Images/Atharv.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+
           <Router>
+            <div className="main-layout">
+              <Header />
+              <div className="content">
             <Routes>
-              <Route path="/" element={<HomePage/>}/>
+              <Route path="/" element={<HomePage />} />
               <Route path="/home" element={<HomePage/>}/>
               <Route path="/about" element={<AboutPage/>}/>
               <Route path="/courses" element={<CoursesPage/>}/>
@@ -19,11 +43,12 @@ const App = () => {
               <Route path="/admission" element={<AdmissionPage/>}/>
             </Routes>
             <ChatbotComponent />
-          </Router>  
-
-
+            </div>
+            <Footer />
+            </div>
+          </Router>              
         </>
-    )
+    );
 }
 
 export default App;
